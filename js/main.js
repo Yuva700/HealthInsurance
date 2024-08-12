1dd0eb1be7a123da5fbbb37a48c6c9a1301eb7bb
Yuva700 <yuvaraj.ts02@gmail.com>
2024-08-12 05:42:18 +0530

    Second Commit

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ function toggleMenu() {
     menu.classList.toggle('show');
 }
 
+/*
+function showConfirmation(event) {
+    event.preventDefault(); // Prevent the form from submitting normally
+    document.getElementById("contactForm").style.display = "none"; // Hide the form
+    document.getElementById("confirmationMessage").style.display = "block"; // Show the confirmation message
+}*/
